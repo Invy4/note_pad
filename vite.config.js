107bb3b587d991c9deae5f7633e8a8d607d38f5c
Invy4 <charanankam6@gmail.com>
2026-04-27 18:09:52 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+   plugins: [react()],
+  base: '/note_pad/'
+})
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
